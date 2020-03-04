dde9a3d93ddef39ab68858ec835af01c5b01cf9c
iromashko <iromashko@me.com>
2020-03-04 06:10:33 +0300

    add events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'name', age: 24 });
+user.set({ name: 'newname', age: 55 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
